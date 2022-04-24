70ff00f8033ac78c5d1d8084af61e05a4a3355da
Stefan Stähli <stefan@fam-staeh.li>
2022-04-24 17:18:16 +0200

    Add support for Digilent Cable Copy Digilent cable data over to production container. Export digilent environment variable. Add docker-compose support and a buildscript for convenient deployment

---
+++ buildimage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# get tar file
+tarfile=$(ls  Xilinx_Vivado_Lab_Lin_*.tar.gz)
+
+# get the version from tar file name
+ifsorig=$IFS
+IFS='_'
+read -ra filenameparts <<< "$tarfile"
+IFS=$ifsorig
+version=${filenameparts[4]}
+echo "Version is $version"
+
+# remove old image
+docker-compose stop && docker-compose rm -f
+docker image rm -f xilinx-hw_server-docker:${version}
+
+# create new image
+docker build --build-arg VIVADO_VERSION=${version} --build-arg VIVADO_TAR_FILE=${tarfile} -t xilinx-hw_server-docker:${version} --no-cache .
+
+# save image in tar file
+docker save xilinx-hw_server-docker:${version} > xhwsd.tar
+
+# copy image to raspberry pi
+scp xhwsd.tar user@ip:/home/user/
+
+# on target -> load image
+ssh user@ip "docker load < xhwsd.tar"
