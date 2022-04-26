9a3a68db9fdf5cbb98a7cf8e9628d4bae6199c8a
Steffen Vogel <post@steffenvogel.de>
2022-04-26 09:27:59 +0200

    simplify and cleanup build script

--- build.sh
+++ build.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# get tar file
-tarfile=$(ls  Xilinx_Vivado_Lab_Lin_*.tar.gz)
+# Get the Vivado installer tar file
+VIVADO_TAR_FILE=$(ls  Xilinx_Vivado_Lab_Lin_*.tar.gz)
 
-# get the version from tar file name
-ifsorig=$IFS
+# Get the version from tar file name
+IFS_ORIG=${IFS}
 IFS='_'
-read -ra filenameparts <<< "$tarfile"
-IFS=$ifsorig
-version=${filenameparts[4]}
-echo "Version is $version"
+read -ra FILENAME_PARTS <<< "${VIVADO_TAR_FILE}"
+IFS=${IFS_ORIG}
 
-# remove old image
-docker-compose stop && docker-compose rm -f
-docker image rm -f ghcr.io/sstaehli/hw_server:${version}
+VIVADO_VERSION=${FILENAME_PARTS[4]}
+echo "Version is ${VIVADO_VERSION}"
 
-# create new image
-docker build --build-arg VIVADO_VERSION=${version} --build-arg VIVADO_TAR_FILE=${tarfile} -t ghcr.io/sstaehli/hw_server:${version} --no-cache .
-
-# copy docker-compose file to raspberry pi
-#scp docker-compose.yml user@ip:/home/user/
+# Create new image
+docker build \
+    --build-arg VIVADO_VERSION=${VIVADO_VERSION} \
+    --build-arg VIVADO_TAR_FILE=${VIVADO_TAR_FILE} \
+    --tag ghcr.io/stv0g/hw_server:${VIVADO_VERSION} \
+    .
